907de10a805745b5af73792760c3b3b9c000ee06
bigtoga <scott.whigham@gmail.com>
2020-01-25 13:08:07 -0600

    Update api_keys.py

--- VacationPy/api_keys.py
+++ VacationPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "2990f208f0f83d6878b07de7537a16fd"
+weather_api_key = "demo"
 
 # Google API Key
-g_key = "AIzaSyDSo6bKpNBpgeoiFVMtIjW34tIyPBy5tgY"
+g_key = "demo"
